8c0d1f92adae3b32d0927882c7ca95922c34a25a
Ben <ben@leipeshit.com>
2014-04-23 17:54:50 +0200

    added font-awesome

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Baasje::Application.routes.draw do
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -39,7 +40,7 @@ Baasje::Application.routes.draw do
   #       get 'recent', on: :collection
   #     end
   #   end
-  
+
   # Example resource route with concerns:
   #   concern :toggleable do
   #     post 'toggle'
@@ -53,4 +54,5 @@ Baasje::Application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
+  root :to => 'pages#index'
 end
