645b8deff8e0f42c12c44b32018f4d8ac638c62a
Ben <ben@leipeshit.com>
2014-04-24 07:24:49 +0200

    font-awesome gem

--- config/application.rb
+++ config/application.rb
@@ -16,8 +16,6 @@ module Baasje
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
     # config.time_zone = 'Central Time (US & Canada)'
     config.assets.precompile += %w[vendor/modernizr.js]
-    config.assets.paths << Rails.root.join('app', 'assets', 'fonts')
-    config.assets.precompile += %w(.svg .eot .woff .ttf)
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
